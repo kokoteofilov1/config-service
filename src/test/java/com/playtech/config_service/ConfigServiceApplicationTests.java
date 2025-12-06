11f66f3bd085ab00895ded1ff843f621549ddde3
Kostadin Teofilov <koko_teofilov@abv.bg>
2025-12-06 21:12:00 +0200

    Initialize Spring Boot project

---
+++ src/test/java/com/playtech/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playtech.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
